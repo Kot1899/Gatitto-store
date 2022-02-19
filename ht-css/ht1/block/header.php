d7a33060c4fcc78f20d40b49ff0b8e13666bf4eb
romanenko <romanenko.vit.kharkiv@gmail.com>
2022-02-19 18:25:25 +0200

    home+goods

---
+++ ht-css/ht1/block/header.php
@@ -0,0 +1,12 @@
+<div class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
+    <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
+        <span class="gg-2">KOT</span>
+    </a>
+    <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+        <a class="me-3 py-2 text-dark text-decoration-none" href="#">Main</a>
+        <a class="me-3 py-2 text-dark text-decoration-none" href="#">Goods</a>
+        <a class="me-3 py-2 text-dark text-decoration-none" href="#">Product details</a>
+        <a class="me-3 py-2 text-dark text-decoration-none" href="#">Basket</a>
+    </nav>
+    <a class="btn btn-outline-primary" href="#">Login</a>
+</div>
